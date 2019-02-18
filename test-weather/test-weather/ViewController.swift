3312e5f0dcc60e59d8816323f0e190b7d88d94b3
Ginés Sánchez <gines.sanchez.merono@gmail.com>
2019-02-18 18:52:14 +0100

    Create empty project from Xcode.

---
+++ test-weather/test-weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test-weather
+//
+//  Created by Ginés Sánchez on 2019-02-18.
+//  Copyright © 2019 Ginés Sánchez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
